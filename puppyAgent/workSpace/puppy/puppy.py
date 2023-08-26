68cf153ff66e3d65a7541f5366267055e8a42a32
GuanQunMu <guanqun.mo@gmail.com>
2023-08-26 02:42:07 +0200

    updated puppy

---
+++ puppyAgent/workSpace/puppy/puppy.py
@@ -0,0 +1,11 @@
+class Puppy:
+    def __init__(self, name, authority={}, discription=""):
+        self.name = name
+        self.authority = authority
+        self.discription = discription
+        self.
+    
+    # authority is a dictionary with:
+    {tools:[],
+     notes:[],
+     }
