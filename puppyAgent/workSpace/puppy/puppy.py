9e63801df0ad7f6ea20c3b8f51ab33f2668d3a2e
GuanQunMu <guanqun.mo@gmail.com>
2023-09-19 23:09:12 +0200

    updated single-puppy

--- puppyAgent/workSpace/puppy/puppy.py
+++ puppyAgent/workSpace/puppy/puppy.py
@@ -1,13 +1,26 @@
 class Puppy:
-    def __init__(self, name, authority={}, discription=""):
+    def __init__(self, name, authorizedTools={}, discription=""):
         self.name = name
         self.discription = discription
-        self.authority = authority
+        self.authorizedTools = authorizedTools
+
+        """
+        name: the name of the agent
+        (for example: "David")
+        discription: the discription of the agent
+        (for example: "an leader agent that are capable of contacting other agents.
+        authorized: the authority of the agent
+        (for example: {"tools":["tool1", "tool2"]})
+        """
+
+    def getName(self):
+        return self.name
     
-    # authority is a dictionary with:
-    '''
-    authority={tools:[],
-     notes:[],
-     }
-    '''
+    def getDiscription(self):
+        return self.discription
+    
+    def getAuthorizedTools(self):
+        return self.authorizedTools
+    
+    
 
