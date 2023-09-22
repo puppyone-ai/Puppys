66a3938238d1f67fe7a7ae5697491ae8f2b57e99
GuanQunMu <guanqun.mo@gmail.com>
2023-09-22 19:25:03 +0200

    :package: rearrange the kwargs function for puppy

--- puppyAgent/workSpace/puppy/puppy.py
+++ puppyAgent/workSpace/puppy/puppy.py
@@ -1,3 +1,5 @@
+from types import MethodType
+
 class Puppy:
     def __init__(self, name, authorizedTools={}, discription="", **kwargs):
         self.name = name
@@ -6,8 +8,12 @@ class Puppy:
 
         
         for key, value in kwargs.items():
-            setattr(self, key, value)
-
+            # if the value is a function or method, bind it to the current instance
+            if callable(value):
+                setattr(self, key, value.__get__(self))
+            # else, set it as a property
+            else:
+                setattr(self, key, value)
         """
         name: the name of the agent
         (for example: "David")
@@ -35,6 +41,22 @@ class Puppy:
     def setAuthorizedTools(self, newAuthorizedTools):
         self.authorizedTools = newAuthorizedTools
 
+    # add a new customized function to the class
+    def addFunction(self, func):
+        setattr(self, func.__name__, MethodType(func, self))
+
+    # update a customized function to the class, same as addFunction
+    def updateFunction(self, func):
+        self.addFunction(func)
+
+    # add a new customized property to the class
+    def addProperty(self, attr_name, value):
+        setattr(self, attr_name, value)
+
+    # update a customized property to the class, same as addProperty
+    def updateProperty(self, attr_name, value):
+        self.addProperty(attr_name, value)
+    
     # run the agent with two threads
     def run(self):
         pass
