b6453235ecaa4154c2cb48459b92823953a8784a
GuanQunMu <guanqun.mo@gmail.com>
2023-09-29 01:27:51 +0200

    :fire:updated actionFlow

--- puppyAgent/workSpace/puppy/puppyFunction/actionFlow.py
+++ puppyAgent/workSpace/puppy/puppyFunction/actionFlow.py
@@ -1,10 +1,13 @@
 import inspect
 import re
+import copy
 
 class Action():
     def __init__(self):
         self.actionFlow = []
         self.functions = []
+        self.currentStep = 0
+        self.task=None
 
     # add a new customized function to the class
     def run(self):
@@ -14,14 +17,14 @@ class Action():
     # translate the code to action flow in JSON format
     def action(self, func):
         def wrapper(*args, **kwargs):
-            source_code = inspect.getsource(func)
-            lines = source_code.split('\n')
+            sourceCode = inspect.getsource(func)
+            lines = sourceCode.split('\n')
             is_comment = False
             comment = ""
             for line in lines:
                 if is_comment:
                     is_comment = False
-                    if '.toDo()' in line:
+                    if '.act()' in line:
                         self.actionFlow.append({"action":comment,"executor":"puppy"})
                     else:
                         self.actionFlow.append({"action":comment,"executor":"human"})
@@ -29,43 +32,53 @@ class Action():
                     comment = line.split('##', 1)[1].strip()
                     is_comment = True
             print(self.actionFlow)
+            self.task= inspect.signature(func).parameters["task"]
+
+            # run the function defined by user
+            self.currentStep = 0
             return func(*args, **kwargs)
+            
         self.functions.append(wrapper)
         return wrapper
     
     # let puppy to run what was planned to be responsibled for puppy
-    def toDo(self):
-
-        if self.actionFlow[0]["executor"] == "puppy":
-            print("action for puppy:",self.actionFlow[0]["action"])
-            self.actionFlow.pop(0)
-        elif self.actionFlow[0]["executor"] == "human":
-            print("action for human:",self.actionFlow[0]["action"])
-            self.actionFlow.pop(0)
-            self.toDo()
+    def act(self):
+        if self.actionFlow[self.currentStep]["executor"] == "puppy":
+            print("action for puppy:",self.actionFlow[self.currentStep]["action"])
+        elif self.actionFlow[self.currentStep]["executor"] == "human":
+            print("action for human:",self.actionFlow[self.currentStep]["action"])
         else:
             print("error")
-            self.actionFlow.pop(0)
-            self.toDo()
+        self.currentStep += 1
+
+    # filling out all the actions in the action flow
+    def taskToActionFlow(self):
+        print(self.actionFlow)
+        print(self.task)
+        print(self.currentStep)
+
+    def actionToTools(self):
+        print(self.actionFlow(self.currentStep))
+
 
 puppy1 = Action()
 
 @puppy1.action
 def ReAct(task="provide the answer to the input question"):   
     ## search for the quesiton @google search @zhihu search
-    puppy1.toDo()
+    puppy1.act()
 
     ## rethink about the answer @rethinker
-    puppy1.toDo()
+    puppy1.act()
 
-    ## 塔克斯的狗粮
+    ## clarify I am still running
     print("now i am here")
 
-
     ## do whatever you want to do 
-    puppy1.toDo()
+    puppy1.act()
 
     print("end")
 
 
 puppy1.run()
+puppy1.plan()
