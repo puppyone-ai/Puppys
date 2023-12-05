8bc5dd82b696e81243c0f308e4a3c8c023f32004
GuanQunMu <guanqun.mo@gmail.com>
2023-12-05 21:00:38 +0800

    :fire: finished GPT pulgin

--- puppyAgent/workSpace/puppy/actionLib/GPT.py
+++ puppyAgent/workSpace/puppy/actionLib/GPT.py
@@ -1,31 +1,34 @@
 import os
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.chat_models import ChatOpenAI
+
 
 
 class GPT():
     def __init__(self, text):
         self.description = "GPT, use when you want to generate text based on the input text by GPT3.5 or GPT4"
         self.example = """
-        # generate text based on the input text
-        text = "how should I intall the package of openAI"
+        # get how to install the package of openAI by GPT4
+        text = "how should I install the package of openAI"
         GPT=GPT(text)
         results = GPT.run()
         """
+        
         self.text = text
+        self.apiKey = "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"
         self.model_name = "gpt-4-1106-preview"
-        self.max_tokens = 100000
+        self.max_tokens = 4000
         self.temperature = 0.7
 
     def modelName(self, model_name):
         self.model_name = model_name
 
-    def run(self):
-        llm=ChatOpenAI(temperature=self.temperature,max_tokens=self.max_tokens,model_name=self.model_name)
-        ChatGPT=LLMChain(llm=llm, prompt= "what's the name of the USA's president?")
-        result=ChatGPT.predict()
+    def apiKey(self, api_key):
+        self.apiKey = api_key
 
+    def run(self):
+        chat_model = ChatOpenAI()
+        result=chat_model.invoke(self.text)
         return result
     
 if __name__ == "__main__":
--- puppyAgent/workSpace/puppy/actionLib/test.py
+++ puppyAgent/workSpace/puppy/actionLib/test.py
@@ -1,12 +1,22 @@
-from openai import OpenAI
-client = OpenAI()
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+import os
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
 
-print(completion.choices[0].message)
+ApiKey="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"
+os.environ["OPENAI_API_KEY"]=ApiKey
+
+llm = OpenAI()
+chat_model = ChatOpenAI()
+
+from langchain.schema import HumanMessage
+
+text = "What would be a good company name for a company that makes colorful socks?"
+messages = [HumanMessage(content=text)]
+
+result=llm.invoke(text)
+print(result)
+# >> Feetful of Fun
+
+result=chat_model.invoke(messages)
+print(result)
