7ae3169eb4d66a6073dbce07adae46fddb9ac1a6
GuanQunMu <guanqun.mo@gmail.com>
2024-02-23 23:17:32 +0800

    :wrench: updated sendMessageToHuman.py
    Signed-off-by: GuanQunMu <guanqun.mo@gmail.com>

--- test2.py
+++ test2.py
@@ -14,6 +14,10 @@ XiaoMei = Puppy(name="XiaoMei")
 def actionFlow():
 
     ## 帮我查一下北京的天气
+    beijing_weather ="sunny"
+    XiaoMei.do()
+
+    ## 判断一下 hebei_weather是否是晴天
     XiaoMei.do()
 
     ## 10 秒钟之后告诉我北京的天气是啥
