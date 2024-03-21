13a0a376a01ca24927f9906c5508994a686fa3f7
GuanQunMu <guanqun.mo@gmail.com>
2024-03-21 18:07:08 +0800

    :bug: fixed the bug of mllm
    Signed-off-by: GuanQunMu <guanqun.mo@gmail.com>

--- test.py
+++ test.py
@@ -16,5 +16,9 @@ def actionflow_pending():
     ## send me a message of hello, motherfucker after 10 seconds.
     Xiao_Mei.do()
 
+    ## send the same message to me after 10 seconds
+    Xiao_Mei.do()
+
+
 
 Xiao_Mei.run()
