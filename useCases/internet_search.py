ec3cbd791ece739a5f95d3ceff94764a3a25cf7b
Yuning <neuromancer43@gmail.com>
2024-05-26 20:14:12 +0200

    add search results based summary

--- useCases/internet_search.py
+++ useCases/internet_search.py
@@ -5,17 +5,19 @@ os.environ['PERPLEXITY_API_KEY'] = "pplx-d9c4ae08201dd95c44b14d4726035f696f8ff07
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 from puppy.thread.main import Thread
-
 Mei = Thread()
 
 @Mei.actionflow.update
 def pending_list():
-    ## find me the current weather in Amsterdam and send the message to me
+    ## Go to https://www.coingecko.com/learn/meme-coins-good-bad-ugly and show the HTML
     Mei.do()   
-
-    # print
-
+    ## Then give me a brief about the article and show it to me @gpt
+    Mei.do()
+    ## Today is 2024-5-26, search internet to find the top 10 meme crypto coins ranked by their real-time current market capitalization.
+    ## Search relevant information of the top 10 meme crypto coins, including its current, marketcap, and 24h trading volume. @search
+    Mei.do()
+    ## Make a list using the information of the top 10 meme crypto coins, and show it to me. @gpt
+    Mei.do()
 
 Mei.run()
