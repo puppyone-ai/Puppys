2a41dad36a2845f3c80a37bfdc52046272fc6fb0
GuanQunMu <guanqun.mo@gmail.com>
2024-06-19 23:05:18 +0800

    :wrench: a game demo
    Signed-off-by: GuanQunMu <guanqun.mo@gmail.com>

--- user_case/crypto_analysis/data_analysis.py
+++ user_case/crypto_analysis/data_analysis.py
@@ -12,15 +12,15 @@ from puppy.tools.usable_tools import UsableTools
 import pandas as pd
 
 def crypto_analysis_decisiontree(self):
-    self.tool_box=UsableTools()
 
     self.btc_data = pd.read_csv('data/btc_data.csv')
     self.eth_data = pd.read_csv('data/eth_data.csv')
 
     self.do_check("show me the top 5 lines of the dataframes",show_response= True)
 
-    self.do_check("calculate the correlation function between BTC and ETH price(not only 5 lines, but all data), plot it, and send the result to me",show_response= True)
+    print(self.btc_data.head)
 
+    self.do_check("calculate the correlation function between BTC and ETH price(not only 5 lines, but all data), plot it, and send the result to me",show_response= True)
 
 hacker_news = Puppy(decisiontree=crypto_analysis_decisiontree)
 
