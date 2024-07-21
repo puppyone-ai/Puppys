8d3c563d1ece12f80600ad17116d93e00f1fd669
Yuning <neuromancer43@gmail.com>
2024-07-22 00:33:36 +0200

    add dependencies and update `make.py`

--- docs/make.py
+++ docs/make.py
@@ -1,5 +1,5 @@
 ## please help write this make,py to automate the documentation build process
-import puppy
+import puppys
 import os
 
 os.system("pdoc puppys -o docs --logo '../assets/PuppyHorizon.png'")
